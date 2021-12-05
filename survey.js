91d238bb91cfe801009b5dd7b7e9aa9d80627000
Caseyann <goore_44@hotmail.ca>
2021-12-04 23:08:56 -0500

    survey

---
+++ survey.js
@@ -0,0 +1,43 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+const answers = [];
+
+rl.question('What\'s your name? Nicknames are also acceptable ', (answer) => {
+  answers.push(answer);;
+  rl.question('What\'s an activity you like doing? ', (answer) => {
+  answers.push(answer);
+    rl.question('What do you listen to while doing that? ', (answer) => {
+    answers.push(answer);
+     rl.question('Which meal is your favourite (eg: dinner, brunch, etc.) ', (answer) => {
+      answers.push(answer);
+      rl.question('What\'s your favourite thing to eat for that meal? ', (answer) => {
+        answers.push(answer);
+        rl.question('Which sport is your absolute favourite? ', (answer) => {
+          answers.push(answer);
+          rl.question('What is your superpower? In a few words, tell us what you are amazing at! ', (answer) => {
+            answers.push(answer);
+            rl.close();
+
+
+            const name = answers[0];
+            const activity = answers[1];
+            const music = answers[2];
+            const meal = answers[3];
+            const food = answers[4];
+            const sport = answers[5];
+            const superpower = answers[6];
+
+            console.log();
+            console.log('Your profile is ready!');
+            console.log(`${name} loves listening to ${music}, while ${activity}, devouring ${food} for ${meal}, prefers ${sport} over any other sport, and is amazing at ${superpower}.`);
+          });
+         });
+        });
+      });
+    });
+  });
+});
